d0c2b49f909684e611d2e25c3d1180e5adc97ed6
ranamusab1 <23-CSE-24@students.duet.edu.pk>
2024-01-13 18:36:30 +0500

    rendom number generator

---
+++ script.js
@@ -0,0 +1,19 @@
+function generateRandomNumber() {
+    // Get a random number between 1 and 100 (adjust as needed)
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+    // Display the random number
+    document.getElementById('randomNumber').textContent = '+923012290505' + randomNumber;
+    // Display the random number
+    document.getElementById('randomNumber').textContent = '+923020128168' + randomNumber;
+    // Display the random number
+    document.getElementById('randomNumber').textContent = '+923270599158' + randomNumber;
+    // Display the random number
+    document.getElementById('randomNumber').textContent = '+923194965891' + randomNumber;
+    // Display the random number
+    document.getElementById('randomNumber').textContent = '+923114739840' + randomNumber;
+    // Display the random number
+    document.getElementById('randomNumber').textContent = '+923157552488' + randomNumber;
+    // Display the random number
+    document.getElementById('randomNumber').textContent = '+923192025269' + randomNumber;
+}
